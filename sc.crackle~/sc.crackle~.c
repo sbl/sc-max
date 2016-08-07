754f38cb1a1c485db0341a6a270f3637d0d75371
v7b1 <vboehm@gmx.ch>
2016-08-07 22:18:33 +0200

    updated crackle

--- sc.crackle~/sc.crackle~.c
+++ sc.crackle~/sc.crackle~.c
@@ -26,6 +26,12 @@
  
  part of sc-max http://github.com/sbl/sc-max
  see README
+ 
+ *
+ **
+ ***		64bit update by volker boehm, august 2016
+ **
+ *
 */
 
 #include "ext.h"
@@ -39,9 +45,9 @@ typedef struct _crackle
 	t_pxobject      ob;
     short           m_connected;
     
-    float           m_param;
-    float           m_y1;
-    float           m_y2;
+    t_double           m_param;
+    t_double           m_y1;
+    t_double           m_y2;
 } t_crackle;
 
 t_class *crackle_class;
@@ -57,14 +63,20 @@ void crackle_int(t_crackle *x, long param);
 void crackle_dsp(t_crackle *x, t_signal **sp, short *count);
 t_int *crackle_perform(t_int *w);
 
+void crackle_dsp64(t_crackle *x, t_object *dsp64, short *count, double samplerate,
+				 long maxvectorsize, long flags);
+void crackle_perform64(t_crackle *x, t_object *dsp64, double **ins, long numins,
+					 double **outs, long numouts, long sampleframes, long flags, void *userparam);
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int main(void){	
+int C74_EXPORT main(void){	
 	t_class *c;
     
 	c = class_new("sc.crackle~", (method)crackle_new, (method)dsp_free, (long)sizeof(t_crackle), 0L, A_DEFFLOAT, 0);
 	
 	class_addmethod(c, (method)crackle_dsp,         "dsp",      A_CANT, 0);
+	class_addmethod(c, (method)crackle_dsp64,		"dsp64", A_CANT, 0);
 	class_addmethod(c, (method)crackle_assist,      "assist",	A_CANT, 0);
     class_addmethod(c, (method)crackle_float,       "float",    A_FLOAT, 0);
     class_addmethod(c, (method)crackle_int,         "int",      A_LONG, 0);
@@ -97,18 +109,18 @@ t_int *crackle_perform(t_int *w){
     t_float *out = (t_float *)(w[3]);
 	int n = (int)w[4];
     
-    float y1 = x->m_y1;
-	float y2 = x->m_y2;
-	float y0;
+    t_double y1 = x->m_y1;
+	t_double y2 = x->m_y2;
+	t_double y0;
     
-    if (param < 0.f) param = 0.f;
-    if (param > 2.f) param = 2.f;
+    if (param < 0.) param = 0.;
+    if (param > 2.) param = 2.;
     
     if (x->ob.z_disabled)
         return w + 5;    
         
 	while (n--){
-		*out++ = y0 = fabs(y1 * param - y2 - 0.05f);        
+		*out++ = y0 = fabs(y1 * param - y2 - 0.05);
         y2 = y1; y1 = y0;
     }
     
@@ -117,6 +129,42 @@ t_int *crackle_perform(t_int *w){
 
     
 	return w + 5;
+}
+
+
+
+//64-bit dsp method
+void crackle_dsp64(t_crackle *x, t_object *dsp64, short *count, double samplerate,
+				   long maxvectorsize, long flags) {
+	x->m_connected = count[0];
+	object_method(dsp64, gensym("dsp_add64"), x, crackle_perform64, 0, NULL);
+}
+
+// 64 bit signal input version
+void crackle_perform64(t_crackle *x, t_object *dsp64, double **ins, long numins,
+					   double **outs, long numouts, long sampleframes, long flags, void *userparam) {
+	
+	t_double param = x->m_connected ? (*(t_float *)(ins[0])) : x->m_param;
+	t_double *out = outs[0];
+	int vs = sampleframes;
+	
+    if (x->ob.z_disabled)
+        return;
+	
+    t_double y1 = x->m_y1;
+	t_double y2 = x->m_y2;
+	t_double y0;
+    
+    if (param < 0.) param = 0.;
+    if (param > 2.) param = 2.;
+    
+	while (vs--){
+		*out++ = y0 = fabs(y1 * param - y2 - 0.05);
+        y2 = y1; y1 = y0;
+    }
+    
+    x->m_y1 = y1;
+	x->m_y2 = y2;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -141,8 +189,12 @@ void *crackle_new(double param){
         outlet_new((t_object *)x, "signal");
         
         x->m_param = param;
-        x->m_y1 = 0.3f;
-        x->m_y2 = 0.f;
+        x->m_y1 = 0.3;
+        x->m_y2 = 0.;
+	}
+	else {
+		object_free(x);
+		x = NULL;
 	}
 	return (x);
 }
