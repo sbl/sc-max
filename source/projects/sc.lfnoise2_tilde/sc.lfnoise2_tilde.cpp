4fba20bae86dc8447ecdc6994bc9a164f25084a5
sbl <stephen.lumenta@gmail.com>
2016-08-14 15:07:29 +0200

    lfnoise2

--- source/projects/sc.lfnoise2_tilde/sc.lfnoise2_tilde.cpp
+++ source/projects/sc.lfnoise2_tilde/sc.lfnoise2_tilde.cpp
@@ -1,183 +1,156 @@
 /*
-	SuperCollider real time audio synthesis system
-    Copyright (c) 2002 James McCartney. All rights reserved.
-	http://www.audiosynth.com
+ SuperCollider real time audio synthesis system
+ Copyright (c) 2002 James McCartney. All rights reserved.
+  http://www.audiosynth.com
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 /*
-
  sc.lfnoise2~
  (c) stephen lumenta under GPL
  http://www.gnu.org/licenses/gpl.html
- 
+
  part of sc-max http://github.com/sbl/sc-max
  see README
-*/
+ */
 
-#include "ext.h"
-#include "ext_obex.h"
-#include "z_dsp.h"
+#include "c74_msp.h"
 #include "scmax.h"
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+using namespace c74::max;
 
-typedef struct _lfnoise 
-{
-	t_pxobject      ob;
+static t_class *lfnoise_class = nullptr;
+
+struct t_lfnoise {
+    t_pxobject ob;
+
+    double m_freq;
+    short m_connected;
+    double m_level, m_slope, m_curve;
+    double m_nextvalue, m_nextmidpt;
+    double m_sr;
+    long m_counter;
+
+    RGen rgen;
+};
+
+void *lfnoise_new(double freq) {
+    t_lfnoise *self = NULL;
+    self = (t_lfnoise *)object_alloc(lfnoise_class);
     
-    float           m_freq;
-    short           m_connected;
-    float           m_level, m_slope, m_curve;
-    float           m_nextvalue, m_nextmidpt;
-    float           m_sr;
-    int             m_counter;
+    if (!self) return self;
     
-    RGen            rgen;
-} t_lfnoise;
-
-t_class *lfnoise_class;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void *lfnoise_new       (double freq);
-void lfnoise_free       (t_lfnoise *x);
-void lfnoise_assist     (t_lfnoise *x, void *b, long m, long a, char *s);
-
-void lfnoise_float      (t_lfnoise *x, double freq);
-
-void lfnoise_dsp        (t_lfnoise *x, t_signal **sp, short *count);
-t_int *lfnoise_perform  (t_int *w);
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-int main(void){	
-	t_class *c;
+    dsp_setup((t_pxobject *)self, 1);
+    outlet_new((t_object *)self, "signal");
     
-	c = class_new("sc.lfnoise2~", (method)lfnoise_new, (method)dsp_free, (long)sizeof(t_lfnoise), 0L, A_DEFFLOAT, 0);
-	
-	class_addmethod(c, (method)lfnoise_dsp,         "dsp",      A_CANT, 0);
-	class_addmethod(c, (method)lfnoise_assist,      "assist",	A_CANT, 0);
-    class_addmethod(c, (method)lfnoise_float,       "float",	A_FLOAT, 0);
+    self->rgen.init(sc_randomSeed());
+    self->m_freq = freq <= 0 ? 500 : freq;
+    self->m_counter = 0;
+    self->m_slope = 0;
+    self->m_level = 0;
+    self->m_nextvalue = self->rgen.frand2();
+    self->m_nextmidpt = self->m_nextvalue * .5f;
+    self->m_sr = sys_getsr();
     
-	class_dspinit(c);				
-	class_register(CLASS_BOX, c);
-	lfnoise_class = c;
-	
-	return 0;
+    return (self);
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void lfnoise_float(t_lfnoise *x, double freq){
-    
-    x->m_freq = (float) freq;
+void lfnoise_float(t_lfnoise *x, double freq) {
+    x->m_freq = freq;
 }
 
-void lfnoise_dsp(t_lfnoise *x, t_signal **sp, short *count){
-    x->m_sr         = sp[0]->s_sr;
-    x->m_connected  = count[0];
-    // class, in, out, n
-	dsp_add(lfnoise_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[1]->s_n);
-}
+void lfnoise_perform64(t_lfnoise* self,
+                       t_object* dsp64,
+                       double** ins,
+                       long numins,
+                       double** outs,
+                       long numouts,
+                       long sampleframes,
+                       long flags,
+                       void* userparam) {
+    
+    double *out = outs[0];
+    int remain = sampleframes;
 
-t_int *lfnoise_perform(t_int *w){
-    t_lfnoise       *x      = (t_lfnoise *) w[1];	
-    t_float         *out    = (t_float *)       w[3];
-	int             remain  = (int)  w[4];
-    
-    t_float         freq    = x->m_connected ? (*(t_float *)(w[2])) : x->m_freq;
-    float           level   = x->m_level;
-	float           slope   = x->m_slope;
-    float           curve   = x->m_curve;
-    int32            counter = x->m_counter;
-    
-    
-    if (x->ob.z_disabled) return w + 5;
-    
-	
-	RGET
-    
-	do {
-		if (counter<=0) {
-			float value = x->m_nextvalue;
-			x->m_nextvalue = frand2(s1,s2,s3);
-			level = x->m_nextmidpt;
-			x->m_nextmidpt = (x->m_nextvalue + value) * 0.5;
-            
-			counter = (int32)(x->m_sr / sc_max(freq, 0.001f));
-			counter = sc_max(2, counter);
-			float fseglen = (float)counter;
-			curve = 2.f * (x->m_nextmidpt - level - fseglen * slope) / (fseglen * fseglen + fseglen);
-		}
-        
-		int nsmps = sc_min(remain, counter);
-		remain -= nsmps;
-		counter -= nsmps;
-        
+    double freq = self->m_connected ? *ins[0] : self->m_freq;
+    double level = self->m_level;
+    double slope = self->m_slope;
+    double curve = self->m_curve;
+    long counter = self->m_counter;
+
+    if (self->ob.z_disabled) return ;
+
+    RGET
+    do {
+        if (counter<=0) {
+            float value = self->m_nextvalue;
+            self->m_nextvalue = frand2(s1,s2,s3);
+            level = self->m_nextmidpt;
+            self->m_nextmidpt = (self->m_nextvalue + value) * 0.5;
+
+            counter = (int32)(self->m_sr / sc_max(freq, 0.001f));
+            counter = sc_max(2, counter);
+            float fseglen = (float)counter;
+            curve = 2.f * (self->m_nextmidpt - level - fseglen * slope) / (fseglen * fseglen + fseglen);
+        }
+
+        int nsmps = sc_min(remain, counter);
+        remain -= nsmps;
+        counter -= nsmps;
+
         while (nsmps--) {
             *out++ = level;
             slope += curve;
-			level += slope;
+            level += slope;
         }
 
-	} while (remain);
+    } while (remain);
 
-	x->m_level  = level;
-	x->m_slope  = slope;
-    x->m_curve  = curve;
-	x->m_counter= counter;
-	
+    self->m_level  = level;
+    self->m_slope  = slope;
+    self->m_curve  = curve;
+    self->m_counter= counter;
     RPUT
-    
-	return w + 5;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void lfnoise_assist(t_lfnoise *x, void *b, long m, long a, char *s)
-{
-	if (m == ASSIST_INLET) { //inlet
-		sprintf(s, "(signal/float) set freq");
-	} 
-	else {	// outlet
-		sprintf(s, "(signal) lfnoise"); 			
-	}
+void lfnoise_dsp64(t_lfnoise *self, t_object* dsp64, short* count, double samplerate, long maxvectorsize, long flags) {
+    self->m_sr = samplerate;
+    self->m_connected  = count[0];
+    object_method_direct(void, (t_object*, t_object*, t_perfroutine64, long, void*),
+                         dsp64, gensym("dsp_add64"), (t_object*)self, (t_perfroutine64)lfnoise_perform64, 0, NULL);
 }
 
-void *lfnoise_new(double freq){
-	t_lfnoise *x = NULL;
-	x = (t_lfnoise *)object_alloc(lfnoise_class);
+void lfnoise_assist(t_lfnoise *x, void *b, t_assist_function io, long a, char *s) {
+    if (io == ASSIST_INLET) { //inlet
+        sprintf(s, "(signal/float) set freq");
+    }
+    else {  // outlet
+        sprintf(s, "(signal) lfnoise");
+    }
+}
+
+void ext_main(void* r) {
+    t_class *c;
+    c = class_new("sc.lfnoise2~", (method)lfnoise_new, (method)dsp_free, (long)sizeof(t_lfnoise), 0L, A_DEFFLOAT, 0);
     
-	if (x) {
-        // 1 inlet
-		dsp_setup((t_pxobject *)x, 1);
-        
-        x->rgen.init(sc_randomSeed());
-        
-        x->m_freq           = freq <= 0 ? 500 : freq;
-        x->m_counter        = 0;
-        x->m_slope          = 0.f;
-        x->m_level          = 0.f;
-        x->m_nextvalue      = x->rgen.frand2();
-        x->m_nextmidpt      = x->m_nextvalue * .5f;
-        x->m_sr             = sys_getsr();
-        
-        
-        outlet_new((t_object *)x, "signal");
-	}
-	return (x);
+    class_addmethod(c, (method)lfnoise_dsp64, "dsp64", A_CANT, 0);
+    class_addmethod(c, (method)lfnoise_assist, "assist", A_CANT, 0);
+    class_addmethod(c, (method)lfnoise_float, "float",  A_FLOAT, 0);
+    
+    class_dspinit(c);
+    class_register(CLASS_BOX, c);
+    lfnoise_class = c;
 }
