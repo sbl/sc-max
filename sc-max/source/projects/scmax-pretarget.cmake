c04fafd55f1b7f4d32282258519c55e4e411e6fa
Stephen Lumenta <stephen.lumenta@gmail.com>
2020-04-10 18:29:53 +0200

    no more sc-max

--- sc-max/source/projects/scmax-pretarget.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 3.0)
-
-string(REGEX REPLACE "(.*)/" "" THIS_FOLDER_NAME "${CMAKE_CURRENT_SOURCE_DIR}")
-project(${THIS_FOLDER_NAME})
-
-include(${C74_MAX_API_DIR}/script/max-pretarget.cmake)
